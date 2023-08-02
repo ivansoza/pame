6fb8d189fb0711f3b97d2fbe00660c1e4265eab4
Ivan <ivansz123@gmail.com>
2023-08-02 12:22:47 -0600

    plantillas

--- pame/vigilancia/views.py
+++ pame/vigilancia/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def homeSeguridadGeneral(request):
+    return render (request, "homeSeguridadGeneral.html")
+
+
+def addAutoridadCompetente(request):
+    return render(request, "addAutoridadCompetente.html")
+
+
+def addAccionMigratoria(request):
+    return render(request, "addAccionMigratoria.html")
+
+def addHospedaje(request):
+    return render(request, "addHospedaje.html")
+
+
+def addTraslado(request):
+    return render(request, "addTraslado.html")
