d10f27b6a2cae037ffb52c63cda521c7d5f2f879
Ivan <ivansz123@gmail.com>
2023-08-10 11:33:04 -0600

    CAMBIOS

--- pame/certificadoMedico/urls.py
+++ pame/certificadoMedico/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-
 from .views import homeCertificadoMedico
+
+# from .views import homeCertificadoMedico
 
 urlpatterns = [
     path('', homeCertificadoMedico, name="homeCertificadoMedico"),
+
 
     
 ]
