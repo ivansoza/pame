1af3669c597a99800817a3680a94dc414fc4bb33
Ivan <ivansz123@gmail.com>
2023-08-11 18:32:08 -0600

    cambios

--- pame/vigilancia/forms.py
+++ pame/vigilancia/forms.py
@@ -2,9 +2,47 @@ from django import forms
 from .models import Extranjero, Acompanante, Nacionalidad, PuestaDisposicionAC, PuestaDisposicionINM, Estacion
 
 class puestDisposicionINMForm(forms.ModelForm):
+    numeroOficio = forms.CharField(
+        label= "Número de Oficio:",
+        widget=forms.TextInput(attrs={'placeholder':'Ej: 162729'})
+    )   
+
+    fechaOficio = forms.DateField(
+        label= "Fecha de Oficio:",
+        widget= forms.DateInput(attrs={'class': 'form-control', 'type': 'date'})
+    )
+
+    nombreAutoridadSigna = forms.CharField(
+        label= "Nombre de Autoridad Asignada:",
+        widget=forms.TextInput(attrs={'placeholder':'Ej: Guillermo Perez Perez'})
+    )
+    cargoAutoridadSigna = forms.CharField(
+        label= "Cargo de Autoridad Asignada:",
+        widget=forms.TextInput(attrs={'placeholder':'Ej: Administrador'})
+    )
+
+    oficioPuesta = forms.FileField(
+         label= "Oficio de Puesta:",
+         # widget=forms.ClearableFileInput(attrs={'class': 'form-control-file'})
+     )
+    
+    oficioComision = forms.FileField(
+         label= "Oficio de Comisión:",
+         widget=forms.ClearableFileInput(attrs={'class': 'form-control-file'})
+     )
+    
+    puntoRevision = forms.CharField(
+        label= "Punto de Revision:",
+        widget=forms.TextInput(attrs={'placeholder':'Ej: Punto 1'})
+    )
+
+
+    
     class Meta:
         model = PuestaDisposicionINM
         fields ='__all__'
+
+
 
 class puestaDisposicionACForm(forms.ModelForm):
     numeroOficio = forms.CharField(
