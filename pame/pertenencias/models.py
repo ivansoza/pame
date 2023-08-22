b7c02de874bb397b7a64546d8605a10ffaec2902
pepe <l1837095@apizaco.tecnm.mx>
2023-08-22 16:00:17 -0600

    COMMIT

--- pame/pertenencias/models.py
+++ pame/pertenencias/models.py
@@ -3,6 +3,7 @@ from vigilancia.models import Extranjero
 # Create your models here.
 
 class Inventario(models.Model):
+    foloInventario = models.CharField(max_length=30, verbose_name='Folio de Inventario')
     unidadMigratoria = models.CharField(max_length=30, verbose_name='Unidad Migratoria')
     fechaEntrega = models.DateField(verbose_name='Fecha Entrega', auto_now_add=True)
     horaEntrega = models.DateTimeField(verbose_name='Hora Entrega', auto_now_add=True)
@@ -10,10 +11,10 @@ class Inventario(models.Model):
     noExtranjero = models.ForeignKey(Extranjero, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return '__all__'
+        return self.foloInventario
 
 class Pertenencias(models.Model):
-    descripcion = models.DateField(max_length=100, verbose_name='Descripcion')
+    descripcion = models.CharField(max_length=100, verbose_name='Descripcion')
     cantidad = models.FloatField(verbose_name='Cantidad')
     observaciones = models.CharField(max_length=100, verbose_name='Obervaciones')
     delInventario =models.ForeignKey(Inventario, on_delete=models.CASCADE, verbose_name='Numero de Inventario')
