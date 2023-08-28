8a56db4a05c0a388fd59232c4357e98fe622c46d
Adair Gustavo <adairgmh@gmail.com>
2023-08-28 09:30:36 -0600

    listar y crear llamadas telefonicas

--- pame/llamadasTelefonicas/models.py
+++ pame/llamadasTelefonicas/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from vigilancia.models import Extranjero
 
 Desea_llamar_choices = [
-    ('1', 'No'),
-    ('2', 'Si'),
+    ('no', 'No'),
+    ('si', 'Si'),
 ]
 
-class llamadasTelefonicas(models.Model):
+class LlamadasTelefonicas(models.Model):
     noExtranjero = models.ForeignKey(Extranjero, on_delete=models.CASCADE)
     estacionMigratoria = models.CharField(max_length=50)
     fechaHoraLlamada = models.DateTimeField()
-    deseaLlamar = models.CharField(max_length=1, choices=Desea_llamar_choices)
+    deseaLlamar = models.CharField(max_length=2, choices=Desea_llamar_choices)
     observaciones =  models.TextField(null=True, blank=True)
     motivo = models.TextField(null=True, blank=True)
     firmaExtranjero = models.FileField(upload_to='files/', null=True, blank=True)
--- pame/llamadasTelefonicas/urls.py
+++ pame/llamadasTelefonicas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
-from .views import homeLLamadasTelefonicas, llamadasTelefonicas
+from .views import homeLLamadasTelefonicas, llamadasTelefonicas, ListLlamadas, crearLlamadas
 
 urlpatterns = [
     # path('', homeLLamadasTelefonicas, name="homeLLamadasTelefonicas"),
-    path('llamadas', llamadasTelefonicas.as_view(), name="llamadasTelefonicas"),
+    # path('llamadas', llamadasTelefonicas.as_view(), name="llamadasTelefonicas"),
+    path('ver-llamadas-imn/<int:llamada_id>/', ListLlamadas.as_view(), name='ver_llamadasIMN'),
+    path('crear-llamada/<int:llamada_id>/', crearLlamadas.as_view(), name='crear_llamada'),
 ]
