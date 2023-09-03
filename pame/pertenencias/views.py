fb4cddab6af7c4a30ec529ab5c9fc7f8705b6044
ivansoza <ivansz123@gmail.com>
2023-09-03 14:31:00 -0500

    PERTENENCIAS

--- pame/pertenencias/views.py
+++ pame/pertenencias/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView
-from vigilancia.models import Extranjero, PuestaDisposicionINM, PuestaDisposicionAC
+from vigilancia.models import Extranjero, PuestaDisposicionINM, PuestaDisposicionAC,PuestaDisposicionVP
 from .forms import InventarioForm, PertenenciaForm, ValoresForm, EnseresForm
 from .models import Pertenencias, Inventario, Valores, EnseresBasicos
 from django.shortcuts import get_object_or_404
@@ -777,3 +777,123 @@ class CrearEnseresModaAC(CreateView):
         form.fields['unidadMigratoria'].widget.attrs['readonly'] = True
         return form
 #-------------------------------FIN --------------------------------
+
+
+#--------------------------------VP--------------------------------
+
+
+    
+class CrearInventarioViewVP(PermissionRequiredMixin,CreateView):
+    permission_required = {
+        'perm1': 'pertenencias.add_inventario',
+    }
+    model = Inventario
+    form_class = InventarioForm
+    template_name = 'pertenenciasVP/agregarInventarioVP.html'
+
+    def form_valid(self, form):
+        extranjero_id = self.kwargs['extranjero_id']
+        form.instance.noExtranjero_id = extranjero_id
+        return super().form_valid(form)
+    def get_initial(self):
+        extranjero_id = self.kwargs['extranjero_id']
+        extranjero = Extranjero.objects.get(id=extranjero_id)
+        estaciones_id = extranjero.deLaEstacion.id
+        estaciones = extranjero.deLaEstacion.nombre
+        ultimo_registro = Inventario.objects.order_by('-id').first()
+        ultimo_numero = int(ultimo_registro.foloInventario.split(f'/')[-1]) if ultimo_registro else 0
+        nuevo_numero = f'2023/INV/{estaciones_id}/{extranjero_id}/{ultimo_numero + 1:06d}'
+
+        return {'noExtranjero': extranjero_id, 'foloInventario':nuevo_numero, 'unidadMigratoria':estaciones}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        extranjero_id = self.kwargs['extranjero_id']
+        puesta_id = self.kwargs.get('puesta_id')
+        extranjero_id1 = self.kwargs['extranjero_id']
+        extranjero = Extranjero.objects.get(id=extranjero_id1)
+        nExtranjero = extranjero.nombreExtranjero
+        apExtranjero = extranjero.apellidoPaternoExtranjero
+        amExtranjero = extranjero.apellidoMaternoExtranjero
+        context['puesta']=PuestaDisposicionINM.objects.get(id=puesta_id)
+        context['extranjero_id1'] = nExtranjero + " " + apExtranjero+" "+amExtranjero
+        context['navbar'] = 'seguridad' 
+        context['seccion'] = 'seguridadVP'
+
+        context['extranjero_id'] = extranjero_id
+        return context
+
+    def get_success_url(self):
+        inventario_id = self.object.id  # Obtiene el ID del inventario recién creado
+        puesta_id = self.kwargs.get('puesta_id')  # Obtiene el ID de la puesta
+        return reverse('ver_pertenenciasVP', kwargs={'inventario_id': inventario_id, 'puesta_id': puesta_id})
+    
+class ListaPertenenciasViewVP(ListView):
+    model = Pertenencias
+    template_name = 'pertenenciasVP/listPertenenciasVP.html'
+
+    def get_queryset(self):
+        inventario_id = self.kwargs['inventario_id']
+        return Pertenencias.objects.filter(delInventario_id=inventario_id)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        inventario_id = self.kwargs['inventario_id']
+        inventario = Inventario.objects.get(pk=inventario_id)
+        puesta_id = self.kwargs.get('puesta_id')
+        context['puesta']=PuestaDisposicionVP.objects.get(id=puesta_id)
+        context['inventario'] = inventario
+        context['navbar'] = 'seguridad' 
+        context['seccion'] = 'seguridadVP'
+        return context
+    
+class CrearPertenenciasViewVP(CreateView):
+    model = Pertenencias
+    form_class = PertenenciaForm  # Usa tu formulario modificado
+    template_name = 'modals/vp/crearPertenenciasVP.html'
+
+    def form_valid(self, form):
+        inventario_id = self.kwargs['inventario_id']
+        form.instance.delInventario_id = inventario_id
+        return super().form_valid(form)
+    def get_initial(self):
+        initial = super().get_initial()
+        inventario_id = self.kwargs['inventario_id']
+        initial['delInventario'] = inventario_id
+        return initial
+    def get_success_url(self):
+        inventario_id = self.kwargs['inventario_id']
+        puesta_id = self.kwargs.get('puesta_id')
+
+        return reverse('ver_pertenenciasVP', kwargs={'inventario_id': inventario_id, 'puesta_id': puesta_id})
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        inventario_id = self.kwargs['inventario_id']
+        inventario = Inventario.objects.get(pk=inventario_id)
+        puesta_id = self.kwargs.get('puesta_id')
+        context['puesta']=PuestaDisposicionVP.objects.get(id=puesta_id)
+        context['inventario'] = inventario
+        context['navbar'] = 'seguridad' 
+        context['seccion'] = 'seguridadVP'
+        return context
+    
+class DeletePertenenciasVP(DeleteView):
+    permission_required = {
+        'perm1': 'vigilancia.delete_pertenencia',
+    }
+    model = Pertenencias
+    template_name = 'modals/vp/eliminarPertenenciaVP.html'
+
+    def get_success_url(self):
+        inventario_id = self.object.delInventario.id
+        puesta_id = self.object.delInventario.noExtranjero.deLaPuestaVP.id
+        return reverse_lazy('ver_pertenenciasVP', args=[inventario_id, puesta_id])
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['navbar'] = 'seguridad'  # Cambia esto según la página activa
+        context['seccion'] = 'seguridadVP'  # Cambia esto según la página activa
+        
+        return context
