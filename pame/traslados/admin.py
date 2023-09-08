50f737dc69486ddd83c2fcde8eb6e9f7ff4d3ca8
ivansoza <ivansz123@gmail.com>
2023-09-07 23:21:06 -0500

    cambios en los static

--- pame/traslados/admin.py
+++ pame/traslados/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Traslado, SolicitudTraslado
 # Register your models here.
 admin.site.register(Traslado)
-admin.site.register(SolicitudTraslado)
+
+
+class SolicitudTrasladoAdmin(admin.ModelAdmin):
+    list_display = ["extranjero", "estacion_origen","estacion_destino","estado","motivo","fecha_solicitud","fecha_aceptacion"]
+admin.site.register(SolicitudTraslado, SolicitudTrasladoAdmin)
