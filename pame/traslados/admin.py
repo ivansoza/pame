e1c3867b3dcf1692c41c53cbefbb515cc3fbabb8
Jose Huerta <l18370954@apizaco.tecnm.mx>
2023-09-11 12:53:17 -0600

    admin traslado

--- pame/traslados/admin.py
+++ pame/traslados/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Traslado, SolicitudTraslado
+from .models import Traslado, SolicitudTraslado, ExtranjeroTraslado, Alojamiento
 # Register your models here.
 admin.site.register(Traslado)
-
+admin.site.register(ExtranjeroTraslado)
+admin.site.register(Alojamiento)
 
 class SolicitudTrasladoAdmin(admin.ModelAdmin):
     list_display = ["extranjero", "estacion_origen","estacion_destino","estado","motivo","fecha_solicitud","fecha_aceptacion"]
