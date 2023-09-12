bd1833300276b56367b9defca480db2b0db1a880
ivansoza <ivansz123@gmail.com>
2023-09-11 22:20:20 -0500

    solucion

--- pame/traslados/views.py
+++ pame/traslados/views.py
@@ -140,7 +140,7 @@ class listarEstaciones(ListView):
 class TrasladoCreateView(CreateView):
     model = Traslado
     form_class = TrasladoForm
-    template_name = 'origen/crearPuestaTraslado.html'  # Este será el nombre del archivo HTML que crearás a continuación.
+    template_name = 'modal/crearPuestaTraslado.html'  # Este será el nombre del archivo HTML que crearás a continuación.
     success_url = reverse_lazy('traslado')  # Ajusta este nombre según tu archivo urls.py
 
     def get_initial(self):
