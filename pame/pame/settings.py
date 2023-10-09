cc9672b83e93d7411a31147d64486339db15c04f
ivansoza <ivansz123@gmail.com>
2023-10-09 10:34:26 -0500

    nuevo settings

--- pame/pame/settings.py
+++ pame/pame/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     'multiselectfield',
     'actuaciones',
-    'acuerdos',
+    # 'acuerdos',
     'amparo',
     'catalogos',
     'certificadoMedico',
