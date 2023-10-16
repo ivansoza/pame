c2bb7a0c7a52c8f3fb154a0dddc62965dc9690fb
ivansoza <ivansz123@gmail.com>
2023-10-16 09:20:51 -0600

    cambios en traslados

--- pame/traslados/forms.py
+++ pame/traslados/forms.py
@@ -1,8 +1,10 @@
 
 from django import forms 
 from .models import Traslado, ExtranjeroTraslado
+from django.core.exceptions import ValidationError
 
 class TrasladoForm(forms.ModelForm):
+    
     class Meta:
         model = Traslado
         fields = ['numeroUnicoProceso', 'estacion_destino', 'nombreAutoridadEnvia', 'numero_camiones']
@@ -11,7 +13,13 @@ class TrasladoForm(forms.ModelForm):
             'numero_camiones': forms.NumberInput(attrs={'min': '1'}),
         }
 
-
+    def clean_numero_camiones(self):
+        numero_camiones = self.cleaned_data.get('numero_camiones')
+        
+        if numero_camiones <= 0:
+            raise ValidationError('El número de camiones debe ser mayor que 0.')
+        
+        return numero_camiones
 
 class EstatusTrasladoForm(forms.ModelForm):
     class Meta:
