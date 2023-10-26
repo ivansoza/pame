f4fa866bd0c2f35f70e22c34de691995c89f8696
ivansoza <ivansz123@gmail.com>
2023-10-26 13:27:51 -0600

    firma quitada en modales que solicita

--- pame/llamadasTelefonicas/models.py
+++ pame/llamadasTelefonicas/models.py
@@ -13,16 +13,16 @@ class LlamadasTelefonicas(models.Model):
     deseaLlamar = models.CharField(max_length=2,choices=Desea_llamar_choices, default='Si')
     observaciones =  models.TextField(null=True, blank=True)
     motivo = models.TextField(null=True, blank=True)
-    firmaExtranjero = models.FileField(upload_to='files/', null=True, blank=True)
-    huellaExtranjero = models.FileField(upload_to='files/', null=True, blank=True)
+    # firmaExtranjero = models.FileField(upload_to='files/', null=True, blank=True)
+    # huellaExtranjero = models.FileField(upload_to='files/', null=True, blank=True)
     nup = models.ForeignKey(NoProceso, on_delete=models.CASCADE)
 
 class Notificacion(models.Model):
     fechaHoraNotificacion = models.DateTimeField(auto_now_add=True, verbose_name='Fecha y hora de la notificación')
     deseaLlamar = models.BooleanField(verbose_name='¿Desea Llamar?')
     motivoNoLlamada =  models.TextField(verbose_name='Motivo')
-    firmaExtranjero = models.FileField(upload_to='files/', null=True, blank=True)
-    huellaExtranjero = models.FileField(upload_to='files/', null=True, blank=True)
+    # firmaExtranjero = models.FileField(upload_to='files/', null=True, blank=True)
+    # huellaExtranjero = models.FileField(upload_to='files/', null=True, blank=True)
     delExtranjero = models.ForeignKey(Extranjero, on_delete=models.CASCADE)
     nup = models.ForeignKey(NoProceso, on_delete=models.CASCADE)
 
