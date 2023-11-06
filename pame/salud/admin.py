f777866f09211a2d41d62cc39b6ff450b13b2333
Jose Huerta <l18370954@apizaco.tecnm.mx>
2023-11-06 10:46:17 -0600

    FECHA Y OTRAS COSAS

--- pame/salud/admin.py
+++ pame/salud/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import ExpedienteMedico, Patologicos
+from .models import PerfilMedico, Consulta, ConsultaExterna, RecetaMedica, DetalleTratamiento, CertificadoMedico, CertificadoExterno
 
-
-admin.site.register(ExpedienteMedico)
-admin.site.register(Patologicos)
+admin.site.register(PerfilMedico)
+admin.site.register(Consulta)
+admin.site.register(ConsultaExterna)
+admin.site.register(RecetaMedica)
+admin.site.register(DetalleTratamiento)
+admin.site.register(CertificadoMedico)
+admin.site.register(CertificadoExterno)
