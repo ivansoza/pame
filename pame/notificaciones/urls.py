cddc7f2a888e1a8e29dfa4c247e0950b2600227f
ivansoza <ivansz123@gmail.com>
2023-11-21 15:17:06 -0600

    nuevos cambios para notificaciones

--- pame/notificaciones/urls.py
+++ pame/notificaciones/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .import views
-from .views import defensoria,notificar,tabladefensores,SubirArchivo,modalnotificar
+from .views import defensoria,notificar,tabladefensores,SubirArchivo,modalnotificar, listExtranjerosComar, listExtranjerosConsulado, listExtranjerosFiscalia
 
 
 urlpatterns = [
@@ -10,4 +10,10 @@ urlpatterns = [
     path('listdefensores/', tabladefensores.as_view(), name='listdefensores'),
     path('modal/', SubirArchivo.as_view(), name='modale'),
     path('listnotificacion/<int:extranjero_id>/<int:defensoria_id>/', modalnotificar.as_view(), name='listnotificacion'),
+    # Notificaciones
+    path("comar/", listExtranjerosComar.as_view(), name="listExtranjerosComar"),
+    path("consulado/", listExtranjerosConsulado.as_view(), name="listExtranjerosConsulado"),
+    path("fiscalia/", listExtranjerosFiscalia.as_view(), name="listExtranjerosFiscalia"),
+
+
 ]
